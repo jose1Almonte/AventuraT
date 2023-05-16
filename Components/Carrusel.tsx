2c3979b28e85c0d8ef6b499ad5db9b0b1742ed88
andyalv5 <andyalvarez@correo.unimet.edu.ve>
2023-05-16 17:59:26 -0400

    j
    jjj

--- Components/Carrusel.tsx
+++ Components/Carrusel.tsx
@@ -1,25 +1,27 @@
 import React, { useRef } from 'react';
 import { View, Image, StyleSheet, Dimensions, TouchableOpacity, FlatList, Animated } from 'react-native';
-import { CustomComponent1, CustomComponent2, CustomComponent3 } from './CarruselComponent';
+import { CustomComponent1, CustomComponent2, CustomComponent3, CustomComponent4 } from './CarruselComponent';
 
 interface CarouselItem {
   id: number;
   component: React.ReactNode;
 }
+const screenWidth = Dimensions.get('window').width;
 
 const carouselItems: CarouselItem[] = [
   { id: 1, component: <CustomComponent1 /> },
   { id: 2, component: <CustomComponent2 /> },
   { id: 3, component: <CustomComponent3 /> },
+  {id: 4, component:<CustomComponent4/>}
 ];
 
 export const Carrousel = () => {
   const scrollX = useRef(new Animated.Value(0)).current;
 
   const renderItem = ({ item, index }: { item: CarouselItem; index: number }) => {
-    const screenWidth = Dimensions.get('window').width;
-    const centerScale = 1.2;
-    const sideScale = 0.8;
+    
+    const centerScale = 1;
+    const sideScale = 0.6;
     const inputRange = [
       (index - 1) * screenWidth,
       index * screenWidth,
@@ -33,13 +35,15 @@ export const Carrousel = () => {
     });
 
     return (
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.contenedor}>
         <Animated.View style={[styles.carouselItem, { transform: [{ scale: transformScale }] }]}>
           {item.component}
         </Animated.View>
       </TouchableOpacity>
     );
   };
+
+  const itemWidth = Dimensions.get('window').width * 0.8;
 
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
@@ -50,11 +54,12 @@ export const Carrousel = () => {
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
+        snapToInterval={itemWidth}
         onScroll={Animated.event(
           [{ nativeEvent: { contentOffset: { x: scrollX } } }],
           { useNativeDriver: true }
         )}
-        contentContainerStyle={{ alignItems: 'center' }}
+        contentContainerStyle={{ alignItems: 'center', justifyContent: 'center', marginHorizontal: screenWidth*0.10}}
       />
     </View>
   );
@@ -64,6 +69,11 @@ const styles = StyleSheet.create({
   carouselItem: {
     width: Dimensions.get('window').width * 0.8,
     height: Dimensions.get('window').height * 0.6,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  contenedor:{
     justifyContent: 'center',
     alignItems: 'center',
   },
--- Components/CarruselComponent.tsx
+++ Components/CarruselComponent.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export const CustomComponent1 = () => (
-  <View style={{ width: 200, height: 200, backgroundColor: 'red' }}>
+  <View style={styles.contenedor}>
     <Text>Componente 1</Text>
     <Image source={require('../images/mas.jpg')}></Image>
   </View>
 );
 
 export const CustomComponent2 = () => (
-    <View style={{ width: 200, height: 200, backgroundColor: 'red' }}>
+    <View style={styles.contenedor}>
       <Text>Componente 2</Text>
       <Image source={require('../images/mas.jpg')}></Image>
     </View>
   );
 
   export const CustomComponent3 = () => (
-    <View style={{ width: 200, height: 200, backgroundColor: 'red' }}>
+    <View style={styles.contenedor}>
       <Text>Componente 3</Text>
       <Image source={require('../images/mas.jpg')}></Image>
     </View>
   );
+
+  export const CustomComponent4 = () => (
+    <View style={styles.contenedor2}>
+      <Text> </Text>
+    </View>
+  );
+
+  const styles = StyleSheet.create({
+    contenedor: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 200,
+        height: 200,
+        backgroundColor: 'red',
+    },
+    contenedor2: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 1,
+        height: 1,
+    },
+
+});
 
