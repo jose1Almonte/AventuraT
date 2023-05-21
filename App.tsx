8e58a6a2570c9185c923bf28d1047b17463ff962
Albany Avila <avila.albany@correo.unimet.edu.ve>
2023-05-21 13:41:47 -0400

    photo profile fixed with props

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import HomeScreen from './Screens/HomeScreen/HomeScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './Screens/LoginScreen/LoginScreen';
-import DetailsScreen from './Screens/DetailsScreen/DetailsScreen';
+import DetailsScreen from './Screens/DetailsScreen/DetailsScreenUser';
 import { UserProfileScreen } from './Screens/UserProfileScreen/UserProfileScreen';
 import BusinessProfileScreen from './Screens/BusinessProfileScreen/BusinessProfileScreen';
 import NavbarScreen from './Screens/NavbarScreen/NavbarScreen';
@@ -20,12 +20,12 @@ export default function App() {
     <UserProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+          {/* <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
           <Stack.Screen name="MobilePaymentScreen" component={MobilePaymentScreen} options={{ headerShown: false }} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
           <Stack.Screen name="DetailsScreen" component={DetailsScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="BusinessProfileScreen" component={BusinessProfileScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} options={{ headerShown: false }} /> */}
+          {/* <Stack.Screen name="BusinessProfileScreen" component={BusinessProfileScreen} options={{ headerShown: false }} /> */}
           <Stack.Screen name="NavbarScreen" component={NavbarScreen} options={{ headerShown: false }} />
           <Stack.Screen name="SearchScreen" component={SearchScreen} options={{ headerShown: false }} />
           <Stack.Screen name="FavoriteScreen" component={FavoriteScreen} options={{ headerShown: false }} />
