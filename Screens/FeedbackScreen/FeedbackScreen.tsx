612c838af7700747fa28befa8a6fadb2d563be5e
Jenderson Quintero <65185726+JendersonQuintero@users.noreply.github.com>
2023-05-21 11:31:13 -0400

    Creación de pantallas de feedback y rating

---
+++ Screens/FeedbackScreen/FeedbackScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class FeedbackScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>FeedbackScreen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
