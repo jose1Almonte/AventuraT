6aef17e015a60310a9abf69405c30f31921cf582
Albany Avila <avila.albany@correo.unimet.edu.ve>
2023-05-21 00:52:11 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -18,9 +18,9 @@ export default function App() {
     <UserProvider>
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
         <Stack.Screen name="MobilePaymentScreen" component={MobilePaymentScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/> */}
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
         <Stack.Screen name="DetailsScreen" component={DetailsScreen} options={{headerShown: false}}/>
         <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} options={{headerShown: false}}/>
         <Stack.Screen name="BusinessProfileScreen" component={BusinessProfileScreen} options={{headerShown: false}}/>
