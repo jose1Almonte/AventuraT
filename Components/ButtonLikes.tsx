40975c0140bab48197f3d8b7ed5f53c975ccd171
Jenderson Quintero <65185726+JendersonQuintero@users.noreply.github.com>
2023-05-23 01:59:46 -0400

    Merge y correcion de errores

--- Components/ButtonLikes.tsx
+++ Components/ButtonLikes.tsx
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Alert} from 'react-native';
-import {SvgXml} from 'react-native-svg';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { SvgXml } from 'react-native-svg';
 import favorites from '../vectores/favorites';
 import auth from '@react-native-firebase/auth';
 import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
-import storage, {deleteObject, ref} from '@react-native-firebase/storage';
-import { user } from '../firebase/gmail';
+// import storage, {deleteObject, ref} from '@react-native-firebase/storage';
+// import { user } from '../firebase/gmail';
 
-interface ButtonLikesProps{
-  packageDetails:any,
+interface ButtonLikesProps {
+  packageDetails: any,
 }
 
-export function ButtonLikes({packageDetails}:ButtonLikesProps) {
+export function ButtonLikes({ packageDetails }: ButtonLikesProps) {
   const [isClicked, setIsClicked] = useState(false); // Estado para rastrear si se ha hecho clic en el botón
   const handleButtonClick = () => {
     setIsClicked(!isClicked); // Invertir el estado de isClicked al hacer clic
@@ -25,11 +25,11 @@ export function ButtonLikes({packageDetails}:ButtonLikesProps) {
         .collection('users')
         .where('email', '==', user.email)
         .get();
-  
+
       querySnapshot.forEach((doc) => {
         const userData = doc.data();
         let newFavorites = [...userData.favorites];
-  
+
         if (isClicked) {
           // Remove package from favorites
           newFavorites = newFavorites.filter((id) => id !== packageDetails.id);
@@ -37,7 +37,7 @@ export function ButtonLikes({packageDetails}:ButtonLikesProps) {
           // Add package to favorites
           newFavorites.push(packageDetails.id);
         }
-  
+
         firestore()
           .collection('users')
           .doc(doc.id)
@@ -53,7 +53,7 @@ export function ButtonLikes({packageDetails}:ButtonLikesProps) {
       });
     }
   };
-  
+
 
   const heartColor = isClicked ? '#FF3D00' : 'white'; // Establecer el color del ícono de corazón según el estado
 
@@ -61,7 +61,7 @@ export function ButtonLikes({packageDetails}:ButtonLikesProps) {
     <path fill="${heartColor}" d="M12 20.853l-1.577-1.422C4.017 14.607 0 11.189 0 7.038 0 3.364 2.687 1 6 1c1.905 0 3.633.879 5 2.252C11.367 1.879 13.095 1 15 1c3.313 0 6 2.364 6 6.038 0 4.151-4.017 7.569-10.423 12.393L12 20.853z"/>
   </svg>`;
   return (
-    <TouchableOpacity onPress={()=>{handleButtonClick();handleToggled();}} style={styles.button}>
+    <TouchableOpacity onPress={() => { handleButtonClick(); handleToggled(); }} style={styles.button}>
       <SvgXml xml={heartIcon} width={24} height={24} />
     </TouchableOpacity>
   );
--- firebase/Firestore.tsx
+++ firebase/Firestore.tsx
@@ -5,109 +5,116 @@ const usersCollection = firestore().collection('users');
 const usersCollection2 = firestore().collection('enterprise');
 const packagesCollection = firestore().collection('package');
 
-export const addUser = async (array:string[],displayName:string,email:string, emailVerified:boolean,photoURL:string) => {
-    await usersCollection.add({
-        displayName: displayName,
-        email: email,
-        emailVerified: emailVerified,
-        favorites:array,
-        photoURL: photoURL});
+export const addUser = async (array: string[], displayName: string, email: string, emailVerified: boolean, photoURL: string) => {
+  await usersCollection.add({
+    displayName: displayName,
+    email: email,
+    emailVerified: emailVerified,
+    favorites: array,
+    photoURL: photoURL
+  });
 };
 
-export const updateUser = async (array:string[],userId: string, displayName:string,email:string, emailVerified:boolean,photoURL:string) => {
-    await usersCollection.doc(userId).set({
-        displayName: displayName,
-        email: email,
-        emailVerified: emailVerified,
-        favorites:array,
-        photoURL: photoURL});
+export const updateUser = async (array: string[], userId: string, displayName: string, email: string, emailVerified: boolean, photoURL: string) => {
+  await usersCollection.doc(userId).set({
+    displayName: displayName,
+    email: email,
+    emailVerified: emailVerified,
+    favorites: array,
+    photoURL: photoURL
+  });
 };
 
 
-export const addEnterprise = async (enterpriseName:string, rif:string, personResponsible:string ) => {
-    await usersCollection2.add({
-        enterpriseName: enterpriseName,
-        rif: rif,
-        personResponsible: personResponsible});
+export const addEnterprise = async (enterpriseName: string, rif: string, personResponsible: string) => {
+  await usersCollection2.add({
+    enterpriseName: enterpriseName,
+    rif: rif,
+    personResponsible: personResponsible
+  });
 };
 
-export const updateEnterprise = async (enterpriseId: string, enterpriseName:string, rif:string, personResponsible:string ) => {
-    await usersCollection2.doc(enterpriseId).set({
-        enterpriseName: enterpriseName,
-        rif: rif,
-        personResponsible: personResponsible});
+export const updateEnterprise = async (enterpriseId: string, enterpriseName: string, rif: string, personResponsible: string) => {
+  await usersCollection2.doc(enterpriseId).set({
+    enterpriseName: enterpriseName,
+    rif: rif,
+    personResponsible: personResponsible
+  });
 };
 
 export const getUser = async (userId: string) => {
-    const documentSnapshot = await usersCollection.doc(userId).get();
-    return { id: documentSnapshot.id, ...documentSnapshot.data() };
+  const documentSnapshot = await usersCollection.doc(userId).get();
+  return { id: documentSnapshot.id, ...documentSnapshot.data() };
 };
 
-export const checkIfUserExists = async (email:string) => {
-    const querySnapshot = await usersCollection.where('email', '==', email).get();
-    return !querySnapshot.empty;};
-
+export const checkIfUserExists = async (email: string) => {
+  const querySnapshot = await usersCollection.where('email', '==', email).get();
+  return !querySnapshot.empty;
+};
+// @ts-ignore
 export const addPackage = async (id, name, availability, price, description, mainImageUrl, location, date) => {
-        try {
-          const packageData = {
-            id,
-            name,
-            availability,
-            price,
-            description,
-            mainImageUrl,
-            location,
-            date,
-          };
-      
-          await packagesCollection.doc(id.toString()).set(packageData);
-          console.log('Paquete añadido exitosamente a Firestore');
-        } catch (error) {
-          console.error('Error al añadir el paquete a Firestore:', error);
-        }
-      };
+  try {
+    const packageData = {
+      id,
+      name,
+      availability,
+      price,
+      description,
+      mainImageUrl,
+      location,
+      date,
+    };
 
+    await packagesCollection.doc(id.toString()).set(packageData);
+    console.log('Paquete añadido exitosamente a Firestore');
+  } catch (error) {
+    console.error('Error al añadir el paquete a Firestore:', error);
+  }
+};
+
+// @ts-ignore
 export const checkPackage = async (id) => {
-        try {
-          const packageDoc = await packagesCollection.doc(id.toString()).get();
-          return packageDoc.exists;
-        } catch (error) {
-          console.error('Error al comprobar el paquete en Firestore:', error);
-          return false;
-        }
-      };
+  try {
+    const packageDoc = await packagesCollection.doc(id.toString()).get();
+    return packageDoc.exists;
+  } catch (error) {
+    console.error('Error al comprobar el paquete en Firestore:', error);
+    return false;
+  }
+};
 
-      export const uploadImage = async (imagePath, filename) => {
-        try {
-          const response = await fetch(imagePath);
-          const blob = await response.blob();
-          const storageRef = storage().ref().child(filename);
-          await storageRef.put(blob);
-          const downloadUrl = await storageRef.getDownloadURL();
-          return downloadUrl;
-        } catch (error) {
-          console.error('Error al subir la imagen a Firebase Storage:', error);
-          return null;
-        }
-      };
+// @ts-ignore
+export const uploadImage = async (imagePath, filename) => {
+  try {
+    const response = await fetch(imagePath);
+    const blob = await response.blob();
+    const storageRef = storage().ref().child(filename);
+    await storageRef.put(blob);
+    const downloadUrl = await storageRef.getDownloadURL();
+    return downloadUrl;
+  } catch (error) {
+    console.error('Error al subir la imagen a Firebase Storage:', error);
+    return null;
+  }
+};
 
 export const getLastPackageId = async () => {
-        try {
-          const querySnapshot = await packagesCollection.orderBy('id', 'desc').limit(1).get();
-          if (!querySnapshot.empty) {
-            const lastPackage = querySnapshot.docs[0].data();
-            return lastPackage.id;
-          }
-        } catch (error) {
-          console.error('Error al obtener el último ID de paquete en Firestore:', error);
-        }
-      
-        return 0; // Si no hay paquetes, devuelve 0 como último ID
+  try {
+    const querySnapshot = await packagesCollection.orderBy('id', 'desc').limit(1).get();
+    if (!querySnapshot.empty) {
+      const lastPackage = querySnapshot.docs[0].data();
+      return lastPackage.id;
+    }
+  } catch (error) {
+    console.error('Error al obtener el último ID de paquete en Firestore:', error);
+  }
+
+  return 0; // Si no hay paquetes, devuelve 0 como último ID
 };
 
 export const getFavorites = async (email: string) => {
   const snapshot = await usersCollection.where("email", "==", email).limit(1).get()
   return snapshot.docs[0].data().favorites
 
-  
+
 }
