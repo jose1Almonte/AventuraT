f31416262959b9ea41402e1e8ccf8f720e93e93a
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-06-04 18:27:04 -0400

    deleteSelectedPackage done
    Ya se puede borrar el paquete seleccionado

--- firebase/DeletePackage.js
+++ firebase/DeletePackage.js
@@ -1,5 +1,6 @@
 import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
+import storage, {deleteObject, ref} from '@react-native-firebase/storage';
 
 export const usersCollection3 = firestore().collection('package');
 
@@ -22,3 +23,22 @@ export const deleteExpiredDocuments = async () => {
         });
 
 };
+
+export const deleteSelectedPackage = async (packageId) => {
+    const querySnapshot = ((await usersCollection3.where('id', '==', packageId).get()));
+
+    querySnapshot.forEach((doc) => {
+
+        const image = doc.data().mainImageUrl;
+
+        const imageRef = firebase.storage().refFromURL(image);
+
+        // console.log(image);
+        console.log('imageRef: ', imageRef);
+        imageRef.delete().then(() => {console.log("La imagen se ha borrado correctamente.");}).catch((error) => {console.error("Error al borrar la imagen:", error);});
+
+        firestore().collection('package').doc(doc.id).delete();
+            console.log('Se borró el documento con el id = ', doc.id);
+    });
+};
+
