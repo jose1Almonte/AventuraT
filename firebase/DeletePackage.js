0ff41a997ab8b5936a870439531da131bcde9131
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-06-04 17:25:39 -0400

    ScrollView added
    He agregado ScrollView para que se pueda ver una cantidad de paquetes ilimitados

--- firebase/DeletePackage.js
+++ firebase/DeletePackage.js
@@ -1,28 +1,5 @@
 import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
-import storage, {deleteObject, ref} from '@react-native-firebase/storage';
-
-// const db = getFirestore();
-
-// export const deleteDocumentByUserId = async (campo, valor) => {
-//   try {
-//     // Crea una consulta para buscar documentos que cumplan con el criterio
-//     const q = query(collection(db, 'package'), where(campo, '==', valor));
-
-//     // Obtiene los documentos que cumplen con la consulta
-//     const querySnapshot = await getDocs(q);
-
-//     // Elimina los documentos encontrados
-//     querySnapshot.forEach((doc) => {
-//       deleteDoc(doc.ref);
-//       console.log('Documento eliminado:', doc.id);
-//       return true;
-//     });
-//   } catch (error) {
-//     console.error('Error al eliminar el documento:', error);
-//     return false;
-//   }
-// };
 
 const usersCollection3 = firestore().collection('package');
 
