7151c69940202d82fec453759f714e5b5f1a4643
andyalv5 <andyalvarez@correo.unimet.edu.ve>
2023-06-08 00:29:30 -0400

    contento estoy
    ya funciona el corazon

--- Components/ButtonLikes.tsx
+++ Components/ButtonLikes.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import favorites from '../vectores/favorites';
@@ -15,13 +15,34 @@ interface ButtonLikesProps {
 export function ButtonLikes({ packageDetails }: ButtonLikesProps) {
   const [isClicked, setIsClicked] = useState(false); // Estado para rastrear si se ha hecho clic en el botón
 
+  useEffect(() => {
+    checkFavoriteStatus();
+  }, []);
+
+  const checkFavoriteStatus = async () => {
+    const user = auth().currentUser;
+    if (user && packageDetails && packageDetails.id) { // Verificar que packageDetails esté definido
+      const querySnapshot = await firestore()
+        .collection('users')
+        .where('email', '==', user.email)
+        .get();
+
+      querySnapshot.forEach((doc) => {
+        const userData = doc.data();
+        const favorites = userData.favorites;
+        const isFavorite = favorites.includes(packageDetails.id.toString());
+        setIsClicked(isFavorite);
+      });
+    }
+  };
+
   const handleButtonClick = () => {
     setIsClicked(!isClicked); // Invertir el estado de isClicked al hacer clic
   };
 
   const handleToggled = async () => {
     const user = auth().currentUser;
-    if (user) {
+    if (user && packageDetails && packageDetails.id) { // Verificar que packageDetails esté definido
       const querySnapshot = await firestore()
         .collection('users')
         .where('email', '==', user.email)
