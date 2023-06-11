c9a19c2276ea3c867806be9b3287f9707c35931d
León Serpa <leon.serpa@correo.unimet.edu.ve>
2023-06-11 02:58:35 -0400

    vista paypremium ligeramente mejorada

--- Screens/PayPremiumScreen/PayPremiumScreen.tsx
+++ Screens/PayPremiumScreen/PayPremiumScreen.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, ScrollView, Dimensions} from 'react-native'
 import currentLog from '../../firebase/UserData';
 import { changePremium } from '../../firebase/Firestore';
+import { Image, SvgXml } from 'react-native-svg';
+import vectorHelpdeskScreen from '../../vectores/vectorHelpdeskScreen';
+
+
 
 export default function PayPremiumScreen() {
     const [paymentRef, setPaymentRef] = useState("");
     const user = currentLog();
 
 
-        
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
 
 
     const checkPay = async () => {
@@ -24,39 +29,145 @@ export default function PayPremiumScreen() {
     }
     return (
         <>
-        <View>
-            <Text>Descripción de lo que hace el paquete premium</Text>
-            <Text>Beneficio 1!</Text>
-            <Text>Beneficio 2!</Text>
-            <Text>Beneficio 3!</Text>
-            <Text>Beneficio 4!</Text>
-        </View>
-        <View style={styles.textBox}>
-            <Text style={styles.paragraph}>
-                Para proceder, por favor, realiza tu pago móvil tomando en cuenta los siguientes datos:
-            </Text>
-            <Text style={styles.bulletText}>• Nro de telefono: 0424 116 61 78</Text>
-            <Text style={styles.bulletText}>• Banco: Banco Venezolano de Crédito</Text>
-            <Text style={styles.bulletText}>• Cédula: V- 27 624 189</Text>
+        <ScrollView style={styles.giantBox}>
+
+            <SvgXml
+                xml={vectorHelpdeskScreen}
+                width={`${screenWidth * 1}`}
+                height={`${screenHeight * 0.511}`}
+                />
+            <View>
+                    <Text>Descripción de lo que hace el paquete premium</Text>
+                    <Text>Beneficio 1!</Text>
+                    <Text>Beneficio 2!</Text>
+                    <Text>Beneficio 3!</Text>
+                    <Text>Beneficio 4!</Text>
             </View>
-        <View >
-            <TextInput
+
+            <View style={styles.secondBigBox}>
+                <View style={styles.firstBigBox}>
+                <Text style={styles.title}>Método de Pago</Text>
+                </View>
+                <Image
+                    style={styles.imageUsed}
+                    source={require('../../images/online-payment.png')}
+                />
+                {/* <Text style={styles.textTotal}>Total a pagar</Text><Text style={styles.textTotal}>Total a pagar</Text> */}
+            </View>
+            
+            <View style={styles.containerPrice}>
+                <Text style={styles.textTotal}>Total a pagar</Text>
+                <Text style={styles.textPrice}>$ 50</Text>
+            </View>
+
+            <View style={styles.thirdBigBox}>
+                <View style={styles.textBox}>
+                <Text style={styles.paragraph}>
+                    Realiza el pago tomando en cuenta los siguientes datos:
+                </Text>
+                <Text style={styles.bulletText}>
+                    • Nro. de teléfono: 0424-1166-178
+                </Text>
+                <Text style={styles.bulletText}>
+                    • Banco: Banco Venezolano de Crédito
+                </Text>
+                <Text style={styles.bulletText}>• Cédula: V- 27.624.189</Text>
+                </View>
+            </View>
+
+            <View style={styles.fourthBigBox}>
+                <TextInput
                 style={styles.inputReferenceNumber}
                 placeholder="Ingrese nro. de referencia"
-                onChangeText={(text: React.SetStateAction<string>) => setPaymentRef(text)}
+                        onChangeText={(text) => {
+                    setPaymentRef(text)
+                    }
+                }
                 />
-            <TouchableOpacity onPress={checkPay}>
-                <Text >Ya pagué</Text>
-            </TouchableOpacity>
-        </View>
+            </View>
+
+            <View style={styles.fifthBigBox}>
+                <TouchableOpacity style={styles.buttonIPaid} onPress={checkPay}>
+                <Text style={styles.textIPaid}>Ya pagué</Text>
+                </TouchableOpacity>
+            </View>
+
+        </ScrollView>
         </>
     )
 }
 
 const styles = StyleSheet.create({
-    textBox: {
+    giantBox: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+    firstBigBox: {
+        marginTop: 10,
+        justifyContent: 'center',
+    },
+    secondBigBox: {
+        flex: 20.375,
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100%',
+    },
+    containerPrice: {
+        // flex: 18,
+        width: '100%',
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginBottom: '4%',
+    },
+
+    thirdBigBox: {
+        flex: 29.375,
+        alignItems: 'center',
+    },
+    fourthBigBox: {
+        flex: 21.25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: '3%',
+    },
+    fifthBigBox: {
+        flex: 11.25,
+        alignItems: 'center',
         marginTop: '3%',
-        width: '88.61%',
+    },
+    textTotal: {
+        fontFamily: 'Poppins-Medium',
+        fontSize: 20,
+        lineHeight: 36,
+        textAlign: 'center',
+        color: '#1881B1',
+    },
+    textPrice: {
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 30,
+        lineHeight: 36,
+        textAlign: 'center',
+        color: '#1881B1',
+        borderBottomColor: '#1881B1',
+        borderBottomWidth: 1,
+        width: 220,
+    },
+    title: {
+        fontFamily: 'Poppins-Medium',
+        fontSize: 28,
+        lineHeight: 36,
+        marginTop: 60,
+        textAlign: 'center',
+        color: '#FFFFFF',
+    },
+    imageUsed: {
+        height: 220,
+        width: 220,
+        marginTop: 50,
+    },
+    textBox: {
+        marginTop: '3.5%',
+        width: '80.61%',
     },
     paragraph: {
         paddingBottom: '3%',
@@ -71,20 +182,28 @@ const styles = StyleSheet.create({
         fontSize: 14,
         textAlign: 'justify',
         color: '#000000',
-        marginLeft: '7%',
+        marginLeft: 15,
+        marginBottom: 8,
     },
     inputReferenceNumber: {
-        width: '88.05%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#1881B1',
-        height: 48,
-        borderRadius: 25,
-        fontWeight: '500',
+        borderBottomColor: '#1881B1',
+        borderBottomWidth: 1,
+        width: 260,
         fontSize: 18,
-        lineHeight: 27,
-        display: 'flex',
         textAlign: 'center',
+    },
+    buttonIPaid: {
+        backgroundColor: '#1881B1',
+        borderRadius: 8,
+        width: 264,
+        height: 52,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textIPaid: {
         color: '#FFFFFF',
-    },    
+        fontFamily: 'Poppins-Bold',
+        fontSize: 16,
+        lineHeight: 19,
+    },
 })
