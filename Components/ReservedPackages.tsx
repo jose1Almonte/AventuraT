30fc7b0548bc9d3a64b5914de16bbfcd88b5fa27
Jenderson Quintero <65185726+JendersonQuintero@users.noreply.github.com>
2023-06-11 21:39:42 -0400

    Cambio de clase a funcion

--- Components/ReservedPackages.tsx
+++ Components/ReservedPackages.tsx
@@ -1,41 +1,39 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Text, Image} from 'react-native';
-import {SvgXml} from 'react-native-svg';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import { SvgXml } from 'react-native-svg';
 import vectorLocation from '../vectores/vectorLocation';
 
-class ReservedPackages extends Component {
-  render() {
-    return (
-        <View style={styles.containerPrim}>
-            <View style={styles.container}>
-                <View style={styles.container1}>
-                <Image
-                    style={styles.img}
-                    source={{
-                    uri: 'https://media.meer.com/attachments/71d38e2818914225a1196a8f1d3ae4961c2d75c9/store/fill/1090/613/1e8eb3a92a4ebbf7b825e3a2b30dce85c5c9fdee0eaee9fe889aed2f7299/Parque-Nacional-Morrocoy-Venezuela.jpg',
-                    }}
-                    alt="photo"
-                />
-                </View>
-                <View style={styles.ContainerText}>
-                <Text style={styles.textPack}>Full-Day Bahia de Cata</Text>
-                <View style={styles.ContainerLocation}>
-                    <SvgXml xml={vectorLocation} height={12} width={12} />
-                    <Text style={styles.textLocation}>Aragua</Text>
-                </View>
-                </View>
-                <Text style={styles.textPack2}>Esperando Confirmación...</Text>
-            </View>
+export const ReservedPackages = () => {
+  return (
+    <View style={styles.containerPrim}>
+      <View style={styles.container}>
+        <View style={styles.container1}>
+          <Image
+            style={styles.img}
+            source={{
+              uri: 'https://media.meer.com/attachments/71d38e2818914225a1196a8f1d3ae4961c2d75c9/store/fill/1090/613/1e8eb3a92a4ebbf7b825e3a2b30dce85c5c9fdee0eaee9fe889aed2f7299/Parque-Nacional-Morrocoy-Venezuela.jpg',
+            }}
+            alt="photo"
+          />
         </View>
-    );
-  }
+        <View style={styles.ContainerText}>
+          <Text style={styles.textPack}>Full-Day Bahia de Cata</Text>
+          <View style={styles.ContainerLocation}>
+            <SvgXml xml={vectorLocation} height={12} width={12} />
+            <Text style={styles.textLocation}>Aragua</Text>
+          </View>
+        </View>
+        <Text style={styles.textPack2}>Esperando Confirmación...</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   containerPrim: {
     display: 'flex',
     marginTop: 25,
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   container: {
@@ -52,7 +50,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     //gap: 6,
     alignItems: 'center',
-    marginBottom:40,
+    marginBottom: 40,
   },
   textPack: {
     color: 'black',
@@ -60,7 +58,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Medium',
   },
   textPack2: {
-    paddingRight:20,
+    paddingRight: 20,
     padding: 2,
     color: 'red',
     fontSize: 12,
