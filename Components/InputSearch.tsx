ba1690952e783df51da7ff06b09c57ff757af603
Albany Avila <avila.albany@correo.unimet.edu.ve>
2023-06-19 19:53:19 -0400

    details x2

--- Components/InputSearch.tsx
+++ Components/InputSearch.tsx
@@ -260,7 +260,7 @@ const styles = StyleSheet.create({
   txt: {
     color: 'white',
     fontFamily: 'Poppins-medium',
-    fontSize: 14,
+    fontSize: 15,
   },
   txtOptions: {
     color: '#d6d6e6',
